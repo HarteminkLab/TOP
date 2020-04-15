0f0591b0bc7e8ebd33742eb3a7fae01def53be0d
kevinlkx <kevinlkx@gmail.com>
2020-04-15 18:00:23 -0500

    test inital empty package

---
+++ R/add_numbers.R
@@ -0,0 +1,15 @@
+#' Add two numbers
+#'
+#' Add two numeric numbers.
+#' @param a numeric number
+#' @param b numeric number
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' add_numbers(10, 5)
+add_numbers <- function(a, b){
+  return(a + b)
+}
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(TOP)
+
+test_check("TOP")
