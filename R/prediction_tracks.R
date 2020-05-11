c773378657be429d5343757622518b6881377448
kevinlkx <kevinlkx@gmail.com>
2020-05-11 12:09:16 -0500

    add functions for genome browser track settings

---
+++ R/prediction_tracks.R
@@ -0,0 +1,56 @@
+#' @title Remove the overlapping site with lower occupancy
+#'
+#' @param predicted_bedgraph.df predicted TF occupancy in bedgraph format
+#'
+#' @return
+sort_merge_overlap_bedgraph <- function(predicted_bedgraph.df){
+
+  predicted_bedgraph_sorted.df <- predicted_bedgraph.df[with(predicted_bedgraph.df, order(chr, start)), ]
+  starts.df <- predicted_bedgraph_sorted.df[-1,]
+  ends.df <- predicted_bedgraph_sorted.df[-nrow(predicted_bedgraph_sorted.df),]
+
+  idx_overlap <- which(starts.df$start - ends.df$end <= 0 & starts.df$chr == ends.df$chr) + 1
+
+  if(length(idx_overlap) == 0){
+    # cat('No overlapping sites \n')
+    return(predicted_bedgraph_sorted.df)
+  }else{
+    # cat(length(idx_overlap), 'overlapping sites \n')
+
+    ## remove the overlapping site with lower occupancy
+    idx_min_occ <- c()
+    for(i in idx_overlap){
+      if(predicted_bedgraph_sorted.df[i-1, 4] < predicted_bedgraph_sorted.df[i, 4]){
+        idx_min_occ <- c(idx_min_occ, i-1)
+      }else{
+        idx_min_occ <- c(idx_min_occ, i)
+      }
+    }
+    # cat('Remove site:', idx_min_occ, '\n')
+    return(sort_merge_overlap_bedgraph(predicted_bedgraph_sorted.df[-idx_min_occ, ]))
+
+  }
+
+}
+
+#' @title Set genome browser track parameters for predictions in bedGraph format
+#'
+#' @param tf_name TF name
+#' @param pwm_id PWM ID
+#' @param cell_type cell type
+#' @param rep_name replicate name
+#' @param type_model model type
+#' @param viewMax upper limit to view TF occupancy in genome browser
+#' @param mycolor color of the track
+#'
+#' @return
+#'
+track_def_line <- function(tf_name, pwm_id, cell_type, rep_name, type_model, viewMax = 100, mycolor = '0,0,0'){
+  track_name <- paste(tf_name, cell_type, rep_name, 'predicted occupancy')
+  track_description <- paste(tf_name, pwm_id, cell_type, rep_name, type_model, 'predicted occupancy')
+  track_options <- paste0(' visibility=full color=', mycolor,
+                          ' altColor=0,0,0 priority=100 autoScale=off alwaysZero=on graphType=bar maxHeightPixels=100:32:16 viewLimits=0:', viewMax)
+
+  track_def <- paste0('track type=bedGraph ', 'name="', track_name, '" description="', track_description,'"', track_options)
+  return(track_def)
+}
