5f0e8db3da82aab85aebf6d9176ad5cd53a0b2f2
kevinlkx <kevinlkx@gmail.com>
2020-06-24 13:09:03 -0500

    change function name

--- tests/testthat/test-predict_occupancy.R
+++ tests/testthat/test-predict_occupancy.R
@@ -1,4 +1,4 @@
-test_that("predict_coef_BH_mean()", {
+test_that("predict_TOP_coef_mean()", {
   data <- data.frame(pwm_score = c(5,10),
                      DNase_1 = c(1,5),
                      DNase_2 = c(20,30),
@@ -7,7 +7,7 @@ test_that("predict_coef_BH_mean()", {
                      DNase_5 = c(2,3))
 
   coef_mean <- c(intercept = 5, pwm_score = 3, c(1, 3, -1, 2, 1))
-  predicted <- predict_coef_BH_mean(data, coef_mean, transform = 'asinh')
+  predicted <- predict_TOP_coef_mean(data, coef_mean, transform = 'asinh')
 
   data_full <- as.matrix(data.frame(intercept = 1, data, check.names = F))
   coefficients <- as.matrix(coef_mean, ncol = 1)
