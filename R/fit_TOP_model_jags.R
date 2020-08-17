8aeed9844159877b28133ba5b75e184a20e008fe
kevinlkx <kevinlkx@gmail.com>
2020-08-17 12:33:58 -0500

    move combine_training_data function

--- R/fit_TOP_model_jags.R
+++ R/fit_TOP_model_jags.R
@@ -1,54 +1,51 @@
-## TOP model in BUGS code for training multiple TFs in multiple cell types
-## It can be extended to model the heterogeneity across replicates
-TOP.model <- function() {
-  for(i in 1:N) {
-    chip[i] ~ dnorm(mu[i],  tau[tf[i], cell_type[i]])
 
-    mu[i] <- alpha[tf[i], cell_type[i]] +
-      pwm[i] * beta1[tf[i], cell_type[i]] +
-      dnase.left2_sum[i] * beta2[tf[i], cell_type[i]] +
-      dnase.left1_sum[i] * beta3[tf[i], cell_type[i]] +
-      dnase.motif_sum[i] * beta4[tf[i], cell_type[i]] +
-      dnase.right1_sum[i] * beta5[tf[i], cell_type[i]] +
-      dnase.right2_sum[i] * beta6[tf[i], cell_type[i]]
-  }
+#' @title Combine training data from TF x cell type combos
+#'
+#' @param tf_cell_table a data frame with TF name, PWM name, cell type, and link to the training data with PWM, DNase and ChIP data.
+#' @param total_partition split the data into partitions (default = 10) and run Gibbs sampling in parallel.
+#' @param n_part which partition to use for training the model
+#'
+#' @return The function returns a data frame of training data with all TFs and cell type combos.
+#' @export
+combine_training_data <- function(tf_cell_table, total_partition = 10, n_part = 1) {
 
-  for(j in 1:n_tfs) {
-    for(k in 1:n_cell_types) {
+  n_data_read = 0
 
-      alpha[j, k] ~ dnorm(Alpha[j], 1)
-      beta1[j, k] ~ dnorm(Beta1[j], 1)
-      beta2[j, k] ~ dnorm(Beta2[j], 1)
-      beta3[j, k] ~ dnorm(Beta3[j], 1)
-      beta4[j, k] ~ dnorm(Beta4[j], 1)
-      beta5[j, k] ~ dnorm(Beta5[j], 1)
-      beta6[j, k] ~ dnorm(Beta6[j], 1)
-      tau[j, k] ~ dgamma(T[j]^2, T[j])
+  combined_data <- list()
+  for(i in 1:nrow(tf_cell_table)) {
 
+    tf_name <- as.character(tf_cell_table$tf_name[i])
+    pwm_name <- as.character(tf_cell_table$pwm_name[i])
+    cell_type <- as.character(tf_cell_table$cell_type[i])
+    data_file <- as.character(tf_cell_table$data_file[i])
+
+    if(!file.exists(data_file)){
+      stop(data_file, 'not exist!')
     }
+
+    data <- read.table(data_file, header = T, stringsAsFactors = F)
+
+    part_length <- as.integer(nrow(data) / total_partition) + 1
+    selection <- (1 + (n_part - 1) * part_length):min(nrow(data), n_part*part_length)
+
+    data <- data[selection, ]
+    cat(tf_name, cell_type, ':', nrow(data), 'sites \n')
+    data$tf <- tf_name
+    data$cell_type <- cell_type
+    combined_data[[ paste(tf_name, cell_type, sep = '|') ]] <- data
+    n_data_read = n_data_read + 1
+
   }
 
-  for(i in 1:n_tfs) {
-    Alpha[i] ~ dnorm(A, 1)
-    Beta1[i] ~ dnorm(B1, 1)
-    Beta2[i] ~ dnorm(B2, 1)
-    Beta3[i] ~ dnorm(B3, 1)
-    Beta4[i] ~ dnorm(B4, 1)
-    Beta5[i] ~ dnorm(B5, 1)
-    Beta6[i] ~ dnorm(B6, 1)
-    T[i] ~ dgamma(TAU^2, TAU)
-  }
+  cat('read', n_data_read, 'datasets\n')
 
-  A ~ dnorm(0, 1)
-  B1 ~ dnorm(0, 1)
-  B2 ~ dnorm(0, 1)
-  B3 ~ dnorm(0, 1)
-  B4 ~ dnorm(0, 1)
-  B5 ~ dnorm(0, 1)
-  B6 ~ dnorm(0, 1)
-  TAU ~ dgamma(1, 1)
+  ## row combine all data
+  combined_data <- do.call(rbind, combined_data)
+  row.names(combined_data) <- NULL
+
+  return(combined_data)
+
 }
-
 
 #' @title Fit TOP model using JAGS with R2jags package
 #'
