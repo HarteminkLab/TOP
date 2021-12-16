82ce1a36e99e224ad11c9c57912e46d6501db933
kevinlkx <kevinlkx@gmail.com>
2021-12-16 17:01:36 -0600

    updated the unit test for predicting occupancy

--- tests/testthat/test-predict_occupancy.R
+++ tests/testthat/test-predict_occupancy.R
@@ -1,18 +1,19 @@
-test_that("predict_TOP_coef_mean()", {
+test_that("predict_TOP() using mean_coef", {
   data <- data.frame(pwm_score = c(5,10),
-                     DNase_1 = c(1,5),
-                     DNase_2 = c(20,30),
-                     DNase_3 = c(1,0),
-                     DNase_4 = c(10,20),
-                     DNase_5 = c(2,3))
+                     bin1 = c(1,5),
+                     bin2 = c(20,30),
+                     bin3 = c(1,0),
+                     bin4 = c(10,20),
+                     bin5 = c(2,3))
 
-  coef_mean <- c(intercept = 5, pwm_score = 3, c(1, 3, -1, 2, 1))
-  predicted <- predict_TOP_coef_mean(data, coef_mean, transform = 'asinh')
+  mean_coef <- c(intercept = 5, pwm_score = 3, bin = c(1, 3, -1, 2, 1))
+
+  TOP_predictions <- predict_TOP(data, mean_coef, logistic.model = FALSE,
+                                      posterior.option = 'mean', transform = 'asinh')
 
   data_full <- as.matrix(data.frame(intercept = 1, data, check.names = F))
-  coefficients <- as.matrix(coef_mean, ncol = 1)
-  predictions <- as.numeric(data_full %*% coefficients)
-  predictions <- sinh(predictions)
+  coefficients <- as.matrix(mean_coef, ncol = 1)
+  correct_predictions <- sinh(as.numeric(data_full %*% coefficients))
 
-  expect_equal(predicted, predictions)
+  expect_equal(TOP_predictions, correct_predictions)
 })
